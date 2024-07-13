511d58eeee7fcc6a0a1a4290e54fcba7a469eccf
gun7741 <gun7741@naver.com>
2024-07-13 16:06:12 +0900

    feat : tailwind 기본 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/reset.css';
+import '@/app/global.css';
 
 const inter = Inter({ subsets: ['latin'] });
 

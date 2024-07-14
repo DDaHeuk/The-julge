c5b9844daab48d6735500fd5c2cded439476ab96
beom <gb3187@naver.com>
2024-07-15 00:10:08 +0900

    feat : footer 작업

--- src/app/(contents)/test11/page.tsx
+++ src/app/(contents)/test11/page.tsx
@@ -5,6 +5,7 @@ import FilledButton from '@/components/button/filledButton';
 import NoFilledButton from '@/components/button/noFilledButton';
 import DisabledButton from '@/components/button/disabledButton';
 import { useState } from 'react';
+import Footer from '@/components/footer';
 
 export default function test11() {
   const [isAuthorized, setIsAuthorized] = useState<boolean>(true);
@@ -16,6 +17,7 @@ export default function test11() {
       <FilledButton width={350} name="로그인 하기" />
       <NoFilledButton width={350} name="로그인 하기" />
       <DisabledButton width={350} name="신청불가" />
+      <Footer />
     </div>
   );
 }

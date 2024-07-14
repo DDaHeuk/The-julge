32343c66fafe2725083557367f7b6808d979a182
gun7741 <gun7741@naver.com>
2024-07-15 01:33:55 +0900

    test : test 코드

--- src/app/(contents)/test22/page.tsx
+++ src/app/(contents)/test22/page.tsx
@@ -1,16 +1,21 @@
 'use client';
 
+import DropDown from '@/components/dropdown';
 import Input from '@/components/input';
 import { useState } from 'react';
 
 export default function test22() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [password, setPassword] = useState('');
+
+  const menuList = ['안녕', '안녕2', '메뉴3', '메뉴4'];
   return (
-    <div>
+    <form>
       <Input variant="email" />
       <Input variant="password" onChange={(e) => setPassword(e.target.value)} />
       <Input variant="passwordVerify" originalPassword={password} />
-    </div>
+      <Input variant="price" className="w-[500px]" />
+      <DropDown menuItems={menuList} />
+    </form>
   );
 }

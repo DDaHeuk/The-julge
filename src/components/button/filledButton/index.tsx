e571d188bee4de84c750efe2b758018e11c4d7fb
beom <gb3187@naver.com>
2024-07-14 09:40:37 +0900

    feat : 공통 버튼 컴포넌트

---
+++ src/components/button/filledButton/index.tsx
@@ -0,0 +1,17 @@
+interface FilledButtonProps {
+  width: number;
+  name: string;
+}
+
+const FilledButton = ({ width, name }: FilledButtonProps) => {
+  return (
+    <button
+      style={{ width: `${width}px` }}
+      className={`bg-[#EA3C12] h-[32px] md:h-[37px] lg:h-[48px]  rounded-[6px] text-white font-bold text-[12px] md:text-[14px] lg:text-[16px] inline-flex py-[14px] justify-center items-center gap-[8px]`}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default FilledButton;

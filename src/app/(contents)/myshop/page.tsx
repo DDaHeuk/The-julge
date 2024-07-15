29e75ddec24be85fa0323ab1e07214ac456193f3
beom <gb3187@naver.com>
2024-07-15 15:16:30 +0900

    feat : 내 가게 페이지 (등록가게 없을때)

---
+++ src/app/(contents)/myshop/page.tsx
@@ -0,0 +1,22 @@
+export default function myShop() {
+  return (
+    <div className="flex  px-[12px] py-[40px] md:px-[32px] md:py-[60px] lg:px-[237px] lg:py-[60px] flex-col items-start gap-[8px]">
+      <div className="flex-col w-[100%]">
+        <span className="text-black font-bold text-[20px] md:text-[28px] mb-[16px] md:mb-[23px]">
+          내 가게
+        </span>
+        <div className="flex flex-col justify-center items-center rounded-[12px] border-gray20 border gap-[16px] md:gap-[24px] py-[60px]  px-[24px]">
+          <span className=" self-stretch text-black text-[14px] md:text-[16px] text-center">
+            내 가게를 소개하고 공고도 등록해 보세요.
+          </span>
+          <button
+            type="button"
+            className="flex w-[140px] h-[37px] md:w-[380px] md:h-[47px] justify-center items-center rounded-[6px] bg-[#EA3C12] px-[20px] py-[10px] md:px-[136px] md:py-[14px] text-white font-bold text-center text-[14px] md:text-[16px]"
+          >
+            가게 등록하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}

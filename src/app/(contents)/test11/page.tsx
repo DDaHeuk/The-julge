3243d12b14ec4911292c3aad2e1542f0b7b4b1fb
beom <gb3187@naver.com>
2024-07-16 16:55:23 +0900

    feat : 가게 정보가 있을때 내 가게 정보 컴포넌트

--- src/app/(contents)/test11/page.tsx
+++ src/app/(contents)/test11/page.tsx
@@ -1,13 +1,9 @@
-import FilledButton from '@/components/button/filledButton';
-import NoFilledButton from '@/components/button/noFilledButton';
-import DisabledButton from '@/components/button/disabledButton';
+import MyShopInfo from '@/components/myShopInfo';
 
 export default function test11() {
   return (
     <div>
-      <FilledButton width={350} name="로그인 하기" />
-      <NoFilledButton width={350} name="로그인 하기" />
-      <DisabledButton width={350} name="신청불가" />
+      <MyShopInfo />
     </div>
   );
 }

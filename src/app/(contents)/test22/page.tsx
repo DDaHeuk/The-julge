b7847b67fd533eb4e947e32d82cbf4fc2e981dcd
gun7741 <gun7741@naver.com>
2024-07-17 15:05:28 +0900

    feat : 공통 Input,dropdown 수정

--- src/app/(contents)/test22/page.tsx
+++ src/app/(contents)/test22/page.tsx
@@ -8,14 +8,15 @@ export default function test22() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [password, setPassword] = useState('');
 
-  const menuList = ['안녕', '안녕2', '메뉴3', '메뉴4'];
+  const menuList = ['안녕', '안녕2', '메뉴3', '메뉴4', '1', '2', '3'];
   return (
     <form>
       <Input variant="email" />
       <Input variant="password" onChange={(e) => setPassword(e.target.value)} />
-      <Input variant="passwordVerify" originalPassword={password} />
+      <Input variant="passwordVerify" originalPassword={password} className="w-[500px]" />
       <Input variant="price" className="w-[500px]" />
-      <DropDown menuItems={menuList} />
+      <DropDown menuItems={menuList} className="w-[500px]" />
+      <Input variant="normal" className="w-[500px]" />
     </form>
   );
 }

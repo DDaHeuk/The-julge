d69ce2a54c54f4e4e2740cfdc17d205309a78e70
gun7741 <gun7741@naver.com>
2024-07-20 14:46:31 +0900

    feat: announcement detail page

---
+++ src/app/(contents)/announcementDetail/[announcementId]/page.tsx
@@ -0,0 +1,9 @@
+import AnnouncementDetailContainer from '@/components/announcementDetailContainer';
+
+export default function AnnouncementDetail() {
+  return (
+    <div className="bg-gray5 h-screen">
+      <AnnouncementDetailContainer />
+    </div>
+  );
+}

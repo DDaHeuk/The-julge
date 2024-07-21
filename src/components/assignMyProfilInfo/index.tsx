dc7a1fd9a0d0ae718d9fa43b21c225f05032ed56
ClownNero <hk5426@nate.com>
2024-07-22 01:52:51 +0900

    rename: profile 폴더명과 파일명 profil로 수정

--- src/components/assignMyProfilInfo/index.tsx
+++ src/components/assignMyProfilInfo/index.tsx
@@ -1,6 +1,5 @@
 import DropDown from '../dropdown';
 import Input from '../input';
-import Image from 'next/image';
 
 const localList: string[] = [
   '서울특별시 송파구',
@@ -13,7 +12,7 @@ const localList: string[] = [
   'dsgsgdg',
 ];
 
-const AssignMyProfileInfo = () => {
+const AssignMyProfilInfo = () => {
   return (
     <form className="flex flex-col gap-[20px] md:gap-[24px] w-[100%]">
       <div className="inline-grid grid-cols-1 md:grid-cols-2 gap-[20px] lg:grid-cols-3">
@@ -35,4 +34,4 @@ const AssignMyProfileInfo = () => {
   );
 };
 
-export default AssignMyProfileInfo;
+export default AssignMyProfilInfo;

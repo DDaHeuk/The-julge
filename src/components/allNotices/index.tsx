8830678812ed9d949b5b43e1d4d656fe5e6a4c7c
beom <gb3187@naver.com>
2024-07-28 16:01:04 +0900

    feat : 정렬 순서에 대한 타입 지정

--- src/components/allNotices/index.tsx
+++ src/components/allNotices/index.tsx
@@ -1,15 +1,15 @@
 import DropDown from '../dropdown';
 import MyPostInfo from '../myPostInfo';
+import { SORTING_OPTIONS } from '@/types/sortingOptions';
 
 const AllNotices = () => {
-  const items = ['마감임박순', '시급많은순', '시간적은순', '가나다순'];
   return (
     <div className="flex px-[12px] md:px-[32px] lg:px-[238px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start gap-[8px]">
       <div className="flex  md:hidden flex-col items-start gap-[16px] w-[100%]">
         <span className="text-[20px] font-bold">전체 공고</span>
         <div className="flex items-start gap-[10px]">
           <DropDown
-            menuItems={items}
+            menuItems={SORTING_OPTIONS}
             className="flex p-[12px] rounded-[5px] items-center w-[105px] h-[30px] bg-gray10 "
           />
           <button className="flex w-[79px] h-[30px] items-center justify-center rounded-[5px] bg-red30 text-white text-[14px] font-bold">
@@ -21,7 +21,7 @@ const AllNotices = () => {
         <span className="text-[20px] md:text-[28px] font-bold">전체 공고</span>
         <div className="flex items-start gap-[10px]">
           <DropDown
-            menuItems={items}
+            menuItems={SORTING_OPTIONS}
             className="flex p-[12px] rounded-[5px] items-center w-[105px] h-[30px] bg-gray10 "
           />
           <button className="flex w-[79px] h-[30px] items-center justify-center rounded-[5px] bg-red30 text-white text-[14px] font-bold">

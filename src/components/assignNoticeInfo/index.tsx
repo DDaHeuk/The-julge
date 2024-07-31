9569cb1acb47a6b183454aa9af17f7b3c15b7079
beom <gb3187@naver.com>
2024-07-31 14:46:51 +0900

    refactor : 상세필터 공고등록 input 수정

--- src/components/assignNoticeInfo/index.tsx
+++ src/components/assignNoticeInfo/index.tsx
@@ -5,7 +5,7 @@ const AssignNoticeInfo = () => {
     <div className="flex flex-col gap-[20px] md:gap-[24px] w-[100%]">
       {/* 시급 + 시작 일시 + 업무시간에 관한 input PC버전 */}
       <div className="hidden lg:inline-flex items-start gap-[20px]">
-        <Input className="w-[100%]" variant="normal" label="시급" />
+        <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
         <Input className="w-[100%]" variant="normal" label="시작 일시" />
         <Input className="w-[100%]" variant="unit" unitLabel="시간" label="업무 시간" />
       </div>
@@ -14,7 +14,7 @@ const AssignNoticeInfo = () => {
       {/* 시급 + 시작 일시 + 업무시간에 관한 input 테블릿버전 */}
       <div className="hidden md:inline-flex lg:hidden flex-col items-start gap-[20px]">
         <div className="inline-flex items-start gap-[20px] w-[100%]">
-          <Input className="w-[100%]" variant="normal" label="시급" />
+          <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
           <Input className="w-[100%]" variant="normal" label="시작 일시" />
         </div>
         <div className="inline-flex items-start gap-[20px] w-[100%]">
@@ -26,7 +26,7 @@ const AssignNoticeInfo = () => {
 
       {/* 시급 + 시작 일시 + 업무시간에 관한 input 모바일버전 */}
       <div className="md:hidden inline-flex flex-col items-start gap-[20px]">
-        <Input className="w-[100%]" variant="normal" label="시급" />
+        <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
         <Input className="w-[100%]" variant="normal" label="시작 일시" />
         <Input className="w-[100%]" variant="unit" unitLabel="시간" label="업무 시간" />
       </div>

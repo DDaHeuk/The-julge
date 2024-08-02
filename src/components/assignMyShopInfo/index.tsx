2148e16d0d7c0fb113ba2eeb6e06ad137ca9dbf1
beom <gb3187@naver.com>
2024-08-02 16:59:19 +0900

    refactor : 적응형 input 코드 수정

--- src/components/assignMyShopInfo/index.tsx
+++ src/components/assignMyShopInfo/index.tsx
@@ -21,38 +21,22 @@ const AssignMyShopInfo = () => {
       {/* 가게 이름 + 분류 input */}
 
       {/* 주소 + 상세주소 + 기본시급에 관한 input 테블릿,PC버전 */}
-      <div className="md:hidden inline-flex flex-col items-start gap-[20px]">
-        <div className="flex flex-col items-start gap-[8px] w-[100%]">
-          <label>주소</label>
-          <DropDown
-            menuItems={LOCATION}
-            className="w-[100%] bg-white h-[58px] border rounded-[6px] border-gray30 py-[16px] px-[20px]"
-          />
+      <div className="inline-flex flex-col items-start gap-[20px]">
+        <div className="w-[100%]  flex flex-col md:flex-row gap-[20px]">
+          <div className="flex flex-col items-start gap-[8px] w-[100%]">
+            <label>주소</label>
+            <DropDown
+              menuItems={LOCATION}
+              className="w-[100%] bg-white h-[58px] border rounded-[6px] border-gray30 py-[16px] px-[20px]"
+            />
+          </div>
+          <Input className="w-[100%] " variant="normal" label="상세주소" />
         </div>
-        <Input className="w-[100%]" variant="normal" label="상세주소" />
-        <Input className="w-full" variant="unit" unitLabel="원" label="기본 시급" />
+        <Input className="w-full md:w-[49%]" variant="unit" unitLabel="원" label="기본 시급" />
       </div>
       {/* 주소 + 상세주소 + 기본시급에 관한 input 테블릿,PC버전 */}
 
-      {/* 주소 + 상세주소 + 기본시급에 관한 input 모바일버전 */}
-      <div className="hidden md:inline-flex flex-row items-start gap-[20px]">
-        <div className="flex flex-col items-start gap-[8px] w-[100%]">
-          <label>주소</label>
-          <DropDown
-            menuItems={LOCATION}
-            className="w-[100%] bg-white h-[58px] border rounded-[6px] border-gray30 py-[16px] px-[20px]"
-          />
-        </div>
-        <Input className="w-[100%]" variant="normal" label="상세주소" />
-      </div>
-
-      <div className="hidden md:inline-flex flex-row items-start gap-[20px]">
-        <Input className="w-full" variant="unit" unitLabel="원" label="기본 시급" />
-        <div className="w-full" />
-      </div>
-      {/* 주소 + 상세주소 + 기본시급에 관한 input 모바일버전 */}
-
-      <div className="inline-flex flex-col items-start gap-[8px] md:w-[483px]">
+      <div className="inline-flex flex-col items-start gap-[8px] md:w-[49%]">
         <span className="text-black text-[16px]">가게 이미지</span>
         <div className="cursor-pointer rounded-[12px] border border-gray30 bg-gray10 w-[100%] h-[200px] py-[68px]">
           <div className="flex flex-col items-center  gap-[11px]">
--- src/components/assignNoticeInfo/index.tsx
+++ src/components/assignNoticeInfo/index.tsx
@@ -3,32 +3,18 @@ import Input from '../input';
 const AssignNoticeInfo = () => {
   return (
     <div className="flex flex-col gap-[20px] md:gap-[24px] w-[100%]">
-      {/* 시급 + 시작 일시 + 업무시간에 관한 input PC버전 */}
-      <div className="hidden lg:inline-flex items-start gap-[20px]">
-        <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
-        <Input className="w-[100%]" variant="dateTime" label="시작 일시" />
-        <Input className="w-[100%]" variant="unit" unitLabel="시간" label="업무 시간" />
-      </div>
-      {/* 시급 + 시작 일시 + 업무시간에 관한 input PC버전 */}
-
-      {/* 시급 + 시작 일시 + 업무시간에 관한 input 테블릿버전 */}
-      <div className="hidden md:inline-flex lg:hidden flex-col items-start gap-[20px]">
-        <div className="inline-flex items-start gap-[20px] w-[100%]">
+      {/* 시급 + 시작 일시 + 업무시간에 관한 input */}
+      <div className="inline-flex flex-col lg:flex-row items-start gap-[20px]">
+        <div className="w-[100%] lg:w-[66.9%] flex flex-col md:flex-row gap-[20px]">
           <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
           <Input className="w-[100%]" variant="dateTime" label="시작 일시" />
         </div>
-        <div className="inline-flex items-start gap-[20px] w-[100%]">
-          <Input className="w-[100%]" variant="unit" unitLabel="시간" label="업무 시간" />
-          <div className="w-[100%]" />
-        </div>
-      </div>
-      {/* 시급 + 시작 일시 + 업무시간에 관한 input 테블릿버전 */}
-
-      {/* 시급 + 시작 일시 + 업무시간에 관한 input 모바일버전 */}
-      <div className="md:hidden inline-flex flex-col items-start gap-[20px]">
-        <Input className="w-[100%]" variant="unit" unitLabel="원" label="시급" />
-        <Input className="w-[100%]" variant="dateTime" label="시작 일시" />
-        <Input className="w-[100%]" variant="unit" unitLabel="시간" label="업무 시간" />
+        <Input
+          className="w-[100%] md:w-[49%] lg:w-[33.1%]"
+          variant="unit"
+          unitLabel="시간"
+          label="업무 시간"
+        />
       </div>
       {/* 시급 + 시작 일시 + 업무시간에 관한 input 모바일버전 */}
 

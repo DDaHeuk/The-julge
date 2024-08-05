719449ac4be87b0bc4d9c43efaae5520bc5e2a08
gun7741 <gun7741@naver.com>
2024-08-05 13:20:19 +0900

    feat: ssr -ing

--- src/app/(contents)/myshop/notices/[noticeId]/page.tsx
+++ src/app/(contents)/myshop/notices/[noticeId]/page.tsx
@@ -1,5 +1,10 @@
+'use client';
+
 import ApplicantList from '@/components/applicantList';
 import NoticeDetailContainer from '@/components/noticeDetailContainer';
+import { useQuery } from '@tanstack/react-query';
+
+import axios from 'axios';
 
 const tableData = [
   { name: '최희문', intro: '최희문임다', phone: '010-0000-0000', status: '신청중' },
@@ -7,7 +12,16 @@ const tableData = [
   { name: '박철수', intro: '박철수에요', phone: '010-2222-2222', status: '완료' },
 ];
 
+const fetchNoticeDetail = async () => {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/shops/9032e147-f582-4a1d-b3f6-85e06b1254c2/notices/d594c985-c207-4585-ac06-48ab20f2b758`,
+  );
+  return response.data;
+};
+
 export default function NoticeDetailPage() {
+  const data = fetchNoticeDetail();
+  console.log(data);
   return (
     <div className="bg-gray5">
       <NoticeDetailContainer memberType="owner">
--- src/app/(contents)/test22/page.tsx
+++ src/app/(contents)/test22/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Button from '@/components/button';
 import DropDown from '@/components/dropdown';
 import Input from '@/components/input';
+import axios from 'axios';
 import { useState } from 'react';
 
 export default function test22() {
@@ -9,14 +11,43 @@ export default function test22() {
   const [password, setPassword] = useState('');
 
   const menuList = ['안녕', '안녕2', '메뉴3', '메뉴4', '1', '2', '3'];
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/shops/9032e147-f582-4a1d-b3f6-85e06b1254c2/notices`,
+        {
+          hourlyPay: 12000,
+          startsAt: '2024-08-31T12:00:00Z',
+          workhour: 5,
+          description: '알바 대타 구함!',
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
     <form>
       <Input variant="email" />
       <Input variant="password" onChange={(e) => setPassword(e.target.value)} />
       <Input variant="passwordVerify" originalPassword={password} className="w-[500px]" />
-      <Input variant="unit" unitLabel="시간" className="w-[500px]" />
+      <Input variant="unit" unitLabel="원" className="w-[500px]" />
       <DropDown menuItems={menuList} className="w-[500px]" />
       <Input variant="normal" className="w-[500px]" />
+      <Button color="filled" onClick={handleClick}>
+        요청
+      </Button>
     </form>
   );
 }

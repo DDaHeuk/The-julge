9f6c2f3895b4b3fe1d9976842f0684da46080bfe
beom <gb3187@naver.com>
2024-08-12 13:52:23 +0900

    feat : 가게 정보 상세 컨테이너 생성

---
+++ src/components/shopDetailContainer/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+import MyShopInfo from '@/components/myShopInfo';
+import NoticeAssignShop from '@/components/noticeAssignShop';
+import NoticeAssignPost from '@/components/noticeAssignPost';
+import MyPost from '@/components/myPost';
+import { useShopId } from '@/stores/storeUserInfo';
+
+const ShopDetailContainer = () => {
+  const { shopId } = useShopId();
+  const [post] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex  px-[12px] py-[40px] md:px-[32px] md:py-[60px] lg:px-[237px] lg:py-[60px] flex-col items-start gap-[8px]">
+        <div className="flex-col w-[100%]">
+          <span className="text-black font-bold text-[20px] md:text-[28px]">내 가게</span>
+          {shopId ? <MyShopInfo /> : <NoticeAssignShop />}
+        </div>
+      </div>
+      {shopId && (
+        <div className="flex  px-[12px] pt-[40px] pb-[80px] md:px-[32px] md:pt-[60px] md:pb-[120px] lg:px-[237px]  flex-col items-start gap-[8px]">
+          <div className="flex-col w-[100%] gap-[16px] md:gap-[32px]">
+            <span className="text-black font-bold text-[20px] md:text-[28px]">
+              {post ? '내가 등록한 공고' : '등록한 공고'}
+            </span>
+            {post ? <MyPost /> : <NoticeAssignPost />}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShopDetailContainer;

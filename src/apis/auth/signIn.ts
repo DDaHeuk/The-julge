cf18312aaf6ec2c33b1ec69d9b611777e9068687
gun7741 <gun7741@naver.com>
2024-08-13 17:29:56 +0900

    fix : 오타 수정

--- src/apis/auth/signIn.ts
+++ src/apis/auth/signIn.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { SignInData, SignInResponse } from '@/types/signInData';
 
-const signUp = async (data: SignInData): Promise<SignInResponse> => {
+const signIn = async (data: SignInData): Promise<SignInResponse> => {
   const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/token`, data);
   return response.data;
 };
 
-export default signUp;
+export default signIn;

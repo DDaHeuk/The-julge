8d1e67495d872ad3d3fdca7ea8df2ca233b04985
ClownNero <ClownNero@gmail.com>
2024-08-14 23:26:59 +0900

    refactor: 이전 페이지로 이동 방식 수정

--- src/components/assignHeader/index.tsx
+++ src/components/assignHeader/index.tsx
@@ -1,15 +1,20 @@
+'use client';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface HeaderProps {
   title: string;
 }
 
 const AssignHeader = ({ title }: HeaderProps) => {
+  const router = useRouter();
+  const handleClose = () => {
+    router.back(); // 이전 페이지로 돌아가기
+  };
   return (
     <div className="flex justify-between items-start self-stretch">
       <span className="text-black text-[20px] md:text-[28px] font-bold">{title}</span>
-      <Link href="myprofile">
+      <button onClick={handleClose}>
         <Image
           className="md:w-[32px] md:h-[32px]"
           src="/icons/close.svg"
@@ -17,7 +22,7 @@ const AssignHeader = ({ title }: HeaderProps) => {
           width={24}
           height={24}
         />
-      </Link>
+      </button>
     </div>
   );
 };

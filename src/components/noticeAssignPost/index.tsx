6f6808d18438f9363582dd6193df408001ee45ee
beom <gb3187@naver.com>
2024-08-15 14:59:44 +0900

    feat : 공고 등록 경로 설정

--- src/components/noticeAssignPost/index.tsx
+++ src/components/noticeAssignPost/index.tsx
@@ -1,15 +1,19 @@
+import Link from 'next/link';
+
 const NoticeAssignPost = () => {
   return (
     <div className="flex flex-col justify-center items-center rounded-[12px] border-gray20 border gap-[16px] md:gap-[24px] py-[60px]  px-[24px] mt-[12px]">
       <span className=" self-stretch text-black text-[14px] md:text-[16px] text-center">
         공고를 등록해 보세요.
       </span>
-      <button
-        type="button"
-        className="flex w-[140px] h-[37px] md:w-[380px] md:h-[47px] justify-center items-center rounded-[6px] bg-[#EA3C12] px-[20px] py-[10px] md:px-[136px] md:py-[14px] text-white font-bold text-center text-[14px] md:text-[16px]"
-      >
-        공고 등록하기
-      </button>
+      <Link href={'/assignnotice'}>
+        <button
+          type="button"
+          className="flex w-[140px] h-[37px] md:w-[380px] md:h-[47px] justify-center items-center rounded-[6px] bg-[#EA3C12] px-[20px] py-[10px] md:px-[136px] md:py-[14px] text-white font-bold text-center text-[14px] md:text-[16px]"
+        >
+          공고 등록하기
+        </button>
+      </Link>
     </div>
   );
 };

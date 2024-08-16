96e397c50cf3c53e7668b96031ca869b691f324e
beom <gb3187@naver.com>
2024-08-16 14:43:31 +0900

    feat : 공고 편집 페이지 생성

--- src/app/(contents)/editmyshop/[shopId]/page.tsx
+++ src/app/(contents)/editmyshop/[shopId]/page.tsx
@@ -11,7 +11,7 @@ const editMyShop = ({ params }: editMyShopProps) => {
   return (
     <div className="flex px-[12px] md:px-[32px] lg:px-[238px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start bg-gray5">
       <div className="flex flex-col items-center gap-[24px] md:gap-[32px] w-[100%]">
-        <AssignHeader title="가게 정보" />
+        <AssignHeader title="가게 정보 편집" />
         <EditMyShopInfo shopId={params.shopId} />
       </div>
     </div>

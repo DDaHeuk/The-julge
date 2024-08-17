9d1ae36172441109ae3e6dba5e8b515375f0a1bd
beom <gb3187@naver.com>
2024-08-17 17:26:14 +0900

    feat : sooner 라이브러리 사용

--- src/hooks/useEditNoticeMutation.ts
+++ src/hooks/useEditNoticeMutation.ts
@@ -1,8 +1,10 @@
 import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query';
 import { EditNoticeInfoData, EditNoticeResponse } from '@/types/editNoticeInfoData';
+import { ErrorResponseData } from '@/types/errorResponseData';
 import { useRouter } from 'next/navigation';
 import editNotice from '@/apis/editNoticeInfo/editNoticeInfo';
 import { toast } from 'sonner';
+import { AxiosError } from 'axios';
 
 // shopId를 포함한 새로운 타입 정의
 interface EditShopVariables {
@@ -11,10 +13,10 @@ interface EditShopVariables {
   noticeId: string;
 }
 
-const useEditNotice = (): UseMutationResult<EditNoticeResponse, Error, EditShopVariables> => {
+const useEditNotice = (): UseMutationResult<EditNoticeResponse, AxiosError, EditShopVariables> => {
   const queryClient = useQueryClient();
   const router = useRouter();
-  return useMutation<EditNoticeResponse, Error, EditShopVariables>({
+  return useMutation<EditNoticeResponse, AxiosError, EditShopVariables>({
     mutationFn: ({ data, shopId, noticeId }) => editNotice(data, shopId, noticeId),
     onSuccess: (data) => {
       toast.success('공고 편집 성공');
@@ -22,6 +24,13 @@ const useEditNotice = (): UseMutationResult<EditNoticeResponse, Error, EditShopV
       router.push(`/myshop/${data.item.shop.item.id}/notices/${data.item.id}`);
     },
     onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data as ErrorResponseData;
+        const errorMessage = errorData.message || '공고 편집 실패'; // 예: `errorData.message`가 정의된 경우 사용
+        toast.error(`공고 편집 실패: ${errorMessage}`);
+      } else {
+        toast.error('공고 편집 실패: 네트워크 오류');
+      }
       console.error('공고 편집 실패', error);
     },
   });

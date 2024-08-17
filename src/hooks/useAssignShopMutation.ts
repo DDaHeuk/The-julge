df8a80b67f368b557a42d4cfc639311edc4a1a7e
beom <gb3187@naver.com>
2024-08-17 19:54:29 +0900

    feat : 가게 등록 sonner 적용

--- src/hooks/useAssignShopMutation.ts
+++ src/hooks/useAssignShopMutation.ts
@@ -3,19 +3,29 @@ import assignShop from '@/apis/assignShopInfo/assignShopInfo';
 import { AssignShopInfoData, AssignShopResponse } from '@/types/assignShopInfoData';
 import { useShopId } from '@/stores/storeUserInfo';
 import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+import { AxiosError } from 'axios';
+import { ErrorResponseData } from '@/types/errorResponseData';
 
-const useAssignShop = (): UseMutationResult<AssignShopResponse, Error, AssignShopInfoData> => {
+const useAssignShop = (): UseMutationResult<AssignShopResponse, AxiosError, AssignShopInfoData> => {
   const router = useRouter();
   const { setShopId } = useShopId();
-  return useMutation<AssignShopResponse, Error, AssignShopInfoData>({
+  return useMutation<AssignShopResponse, AxiosError, AssignShopInfoData>({
     mutationFn: assignShop,
     onSuccess: (data) => {
-      console.log('가게 등록 성공');
+      toast.success('가게 등록 성공');
       const shopId = data.item.id;
       setShopId(shopId);
       router.push(`/myshop/${shopId}`);
     },
     onError: (error) => {
+      if (error.response) {
+        const errorData = error.response.data as ErrorResponseData;
+        const errorMessage = errorData.message || '가게 등록 실패'; // 예: `errorData.message`가 정의된 경우 사용
+        toast.error(`가게 등록 실패: ${errorMessage}`);
+      } else {
+        toast.error('가게 등록 실패: 네트워크 오류');
+      }
       console.error('가게 등록 실패', error);
     },
   });

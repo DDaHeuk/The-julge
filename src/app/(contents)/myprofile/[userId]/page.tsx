1f37a06ad665e45008cd491a5ac9608939775d40
ClownNero <ClownNero@gmail.com>
2024-08-18 21:41:36 +0900

    feat: 알바님의 공고 신청 내역 조회 기능 추가

--- src/app/(contents)/myprofile/[userId]/page.tsx
+++ src/app/(contents)/myprofile/[userId]/page.tsx
@@ -1,6 +1,7 @@
-import getUserInfo from "@/apis/user/getUserInfo";
-import ProfileDetailContainer from "@/components/profileDetailContainer";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import getUserApplications from '@/apis/user/getUserApplications';
+import getUserInfo from '@/apis/user/getUserInfo';
+import ProfileDetailContainer from '@/components/profileDetailContainer';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 interface MyProfileProps {
   params: {
@@ -10,16 +11,25 @@ interface MyProfileProps {
 
 export default async function myProfile({ params }: MyProfileProps) {
   const queryClient = new QueryClient();
-  
+
+  const { userId } = params;
+  const limit = 5;
+  const initialOffset = 0;
+
   await queryClient.prefetchQuery({
-    queryKey: ['profileDetail', params.userId],
-    queryFn: () => getUserInfo(params.userId),
+    queryKey: ['profileDetail', userId],
+    queryFn: () => getUserInfo(userId),
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['myApplications'],
+    queryFn: () => getUserApplications({ userId, offset: initialOffset, limit }),
   });
 
   return (
     <div className="flex flex-col">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <ProfileDetailContainer userId={params.userId}/>
+        <ProfileDetailContainer userId={userId} />
       </HydrationBoundary>
     </div>
   );

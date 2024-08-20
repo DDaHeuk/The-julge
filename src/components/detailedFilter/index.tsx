f7717deec5984644c4c34f839b692be0b92fc155
beom <gb3187@naver.com>
2024-08-20 18:10:20 +0900

    feat : zustand에 상세 필터 정보 적용 시 저장, 초기화, 불러오기

--- src/components/detailedFilter/index.tsx
+++ src/components/detailedFilter/index.tsx
@@ -5,31 +5,65 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { LOCATION } from '@/constant/location';
 import Input from '../input';
 import Button from '../button';
+import { useDetailedFilterData } from '@/stores/storeDetailedFilter';
 
 interface DetailedFilterProps {
   onClose: () => void;
 }
 
 const DetailedFilter = ({ onClose }: DetailedFilterProps) => {
-  const [selectedLocation, setSelectedLocation] = useState<string[]>([]);
+  const {
+    address,
+    startsAtGte,
+    hourlyPayGte,
+    setAddress,
+    setStartsAtGte,
+    setHourlyPayGte,
+    clearFilter,
+  } = useDetailedFilterData();
+
+  //로컬상태관리
+  const [localAddress, setLocalAddress] = useState<string[]>(address);
+  const [localStartsAtGte, setLocalStartsAtGte] = useState(startsAtGte);
+  const [localHourlyPayGte, setLocalHourlyPayGte] = useState<number | undefined>(hourlyPayGte);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'startsAtGte') {
+      setLocalStartsAtGte(value);
+    } else if (name === 'hourlyPayGte') {
+      setLocalHourlyPayGte(Number(value));
+    }
+  };
 
   const handleLocationClick = (location: string) => {
-    setSelectedLocation((prevSelected) => {
-      if (prevSelected.includes(location)) {
-        return [...prevSelected];
-      }
-      return [...prevSelected, location];
-    });
+    if (!localAddress.includes(location)) {
+      setLocalAddress([...localAddress, location]);
+    }
   };
 
   const handleRemoveLocation = (location: string) => {
-    setSelectedLocation((prevSelected) => {
-      return prevSelected.filter((item) => item !== location);
-    });
+    setLocalAddress(localAddress.filter((item) => item !== location));
+  };
+
+  const handleApply = () => {
+    // 로컬 상태를 zustand 스토어에 저장
+    setAddress(localAddress);
+    setStartsAtGte(localStartsAtGte);
+    if (localHourlyPayGte !== undefined) {
+      setHourlyPayGte(localHourlyPayGte);
+    }
+    console.log(address, startsAtGte, hourlyPayGte);
+    onClose();
+  };
+
+  const handleClear = () => {
+    clearFilter();
+    onClose();
   };
 
   return (
@@ -60,7 +94,7 @@ const DetailedFilter = ({ onClose }: DetailedFilterProps) => {
           ))}
         </div>
         <div className="grid grid-cols-2 gap-x-[8px] gap-y-[15px]">
-          {selectedLocation.map((item: string, index) => (
+          {localAddress.map((item: string, index) => (
             <React.Fragment key={index}>
               <div className="flex py-[6px] px-[10px] justify-center items-center gap-1 rounded-[20px] bg-red10">
                 <span className="text-[14px] font-bold text-primary">{item}</span>
@@ -77,18 +111,33 @@ const DetailedFilter = ({ onClose }: DetailedFilterProps) => {
         </div>
       </div>
       <hr className="h-[2px] self-stretch border-gray10" />
-      <Input variant="date" label="시작일" className="w-full" />
+      <Input
+        variant="date"
+        name="startsAtGte"
+        label="시작일"
+        className="w-full"
+        onChange={handleInputChange}
+        value={startsAtGte}
+      />
       <hr className="h-[2px] self-stretch border-gray10" />
       <div className="flex justify-center items-center">
-        <Input variant="unit" unitLabel="원" label="가격" className="w-[169px]" />
+        <Input
+          variant="unit"
+          name="hourlyPayGte"
+          unitLabel="원"
+          label="가격"
+          className="w-[169px]"
+          onChange={handleInputChange}
+          value={hourlyPayGte}
+        />
         <span className="ml-3 mt-5">이상부터</span>
       </div>
       <hr className="h-[2px] self-stretch border-gray10" />
       <div className="flex items-start gap-2 w-full md:mt-[16px]">
-        <Button color="noFilled" className="w-[30%]">
+        <Button color="noFilled" className="w-[30%]" onClick={handleClear}>
           초기화
         </Button>
-        <Button color="filled" className="w-[70%]">
+        <Button color="filled" className="w-[70%]" onClick={handleApply}>
           적용하기
         </Button>
       </div>
---
+++ src/stores/storeDetailedFilter.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface DetailedFilterData {
+  address: string[];
+  startsAtGte: string;
+  hourlyPayGte: number;
+  setAddress: (locations: string[]) => void;
+  setStartsAtGte: (newStartsAt: string) => void;
+  setHourlyPayGte: (newHourlyPay: number) => void;
+  clearFilter: () => void;
+}
+
+export const useDetailedFilterData = create(
+  persist<DetailedFilterData>(
+    (set) => ({
+      address: [],
+      startsAtGte: '',
+      hourlyPayGte: 0,
+      setAddress: (locations: string[]) => set({ address: locations }),
+      setStartsAtGte: (newStartsAt: string) => set({ startsAtGte: newStartsAt }),
+      setHourlyPayGte: (newHourlyPay: number) => set({ hourlyPayGte: newHourlyPay }),
+      clearFilter: () =>
+        set({
+          address: [],
+          startsAtGte: '',
+          hourlyPayGte: 0,
+        }),
+    }),
+    {
+      name: 'detailedFilterStorage',
+    },
+  ),
+);

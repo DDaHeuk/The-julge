959400606673eff6a45df726109a798a9d6a74bb
ClownNero <ClownNero@gmail.com>
2024-08-22 00:50:33 +0900

    feat: 알바 공고 신청 API 기능 추가

--- src/hooks/useEditProfileMutation.ts
+++ src/hooks/useEditProfileMutation.ts
@@ -1,12 +1,15 @@
 import editProfile from '@/apis/editProfileInfo/editProfileInfo';
 import { useUserId } from '@/stores/storeUserInfo';
 import { AssignProfileInfoData, AssignProfileResponse } from '@/types/assignProfileInfoData';
+import { ErrorResponseData } from '@/types/errorResponseData';
 import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 const useEditProfile = (): UseMutationResult<
   AssignProfileResponse,
-  Error,
+  AxiosError,
   AssignProfileInfoData
 > => {
   const { userId } = useUserId();
@@ -15,12 +18,19 @@ const useEditProfile = (): UseMutationResult<
   return useMutation({
     mutationFn: (data: AssignProfileInfoData) => editProfile(userId, data),
     onSuccess: () => {
-      console.log('프로필 수정 성공');
+      toast.success('프로필 편집 성공');
       queryClient.invalidateQueries({ queryKey: ['profileDetail', userId] });
       router.push(`/myprofile/${userId}`);
     },
     onError: (error) => {
-      console.error('프로필 수정 실패', error);
+      if (error.response) {
+        const errorData = error.response.data as ErrorResponseData;
+        const errorMessage = errorData.message || '프로필 편집 실패'; // 예: `errorData.message`가 정의된 경우 사용
+        toast.error(`프로필 편집 실패 : ${errorMessage}`);
+      } else {
+        toast.error(`프로필 편집 실패 : 네트워크 오류`);
+      }
+      console.error('프로필 편집 실패', error);
     },
   });
 };

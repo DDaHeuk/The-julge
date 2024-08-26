5f48eb5e9cfa3b3dc842800cc73e5f374cec9ab7
gun7741 <gun7741@naver.com>
2024-08-26 19:03:09 +0900

    design : pc사이즈 padding값 조정

--- src/app/(contents)/assignmyshop/page.tsx
+++ src/app/(contents)/assignmyshop/page.tsx
@@ -3,7 +3,7 @@ import AssignMyShopInfo from '@/components/assignMyShopInfo';
 
 const assignMyShop = () => {
   return (
-    <div className="flex px-[12px] md:px-[32px] lg:px-[238px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start bg-gray5">
+    <div className="flex px-[12px] md:px-[32px] lg:px-[400px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start bg-gray5">
       <div className="flex flex-col items-center gap-[24px] md:gap-[32px] w-[100%]">
         <AssignHeader title="가게 정보 등록" />
         <AssignMyShopInfo />
--- src/app/(contents)/assignnotice/page.tsx
+++ src/app/(contents)/assignnotice/page.tsx
@@ -3,7 +3,7 @@ import AssignNoticeInfo from '@/components/assignNoticeInfo';
 
 const assignNotice = () => {
   return (
-    <div className="flex h-screen px-[12px] md:px-[32px] lg:px-[238px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start bg-gray5">
+    <div className="flex h-screen px-[12px] md:px-[32px] lg:px-[400px] pt-[40px] md:pt-[60px] pb-[80px] md:pb-[60px] flex-col items-start bg-gray5">
       <div className="flex flex-col items-center gap-[24px] md:gap-[32px] w-[100%]">
         <AssignHeader title="공고 등록" />
         <AssignNoticeInfo />

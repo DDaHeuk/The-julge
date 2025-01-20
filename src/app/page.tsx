b9e0c41bdd82d0ab3f10f57d5e800f0ceed649d9
beom <gb3187@naver.com>
2025-01-20 17:11:28 +0900

    Refactor: 로딩 파일 위치 옮기기 및 suspense 컴포넌트 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import FetchAllNotice from '@/apis/notice/fetchAllNotice';
 import AllNotices from '@/components/homeComponents/allNotices';
 import NavigationBar from '@/components/commonComponents/navigationBar';
-// import { cookies } from 'next/headers';
-import { Suspense } from 'react';
 
 export default async function Home() {
   const queryClient = new QueryClient();
@@ -21,14 +19,10 @@ export default async function Home() {
     <HydrationBoundary state={dehydrate(queryClient)}>
       <div className=" flex-col min-h-screen">
         <div className=" flex-col min-h-[calc(100vh-126px)] md:min-h-[calc(100vh-100px)]">
-          <Suspense>
-            <NavigationBar />
-          </Suspense>
+          <NavigationBar />
           <div className="flex flex-col w-[100%]">
-            <Suspense>
-              <CustomNotice />
-              <AllNotices />
-            </Suspense>
+            <CustomNotice />
+            <AllNotices />
           </div>
         </div>
         <Footer />

5cf9a7377ce56bd60cfc7dc2f1bb8eb55424fd31
beom <gb3187@naver.com>
2025-02-09 20:07:20 +0900

    Refactor: 버튼 로딩 상태 처리를 위한 버튼 공통 컴포넌트 수정

--- src/components/commonComponents/button/index.tsx
+++ src/components/commonComponents/button/index.tsx
@@ -3,12 +3,21 @@
 
 'use client';
 
+import Spinner from '../spinner';
+
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  pending?: boolean;
   color?: 'filled' | 'noFilled';
 }
 
-export default function Button({ children, color, className, ...rest }: ButtonProps) {
+export default function Button({
+  children,
+  color,
+  pending = false,
+  className,
+  ...rest
+}: ButtonProps) {
   const { disabled, onClick, ...restProps } = rest;
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -22,19 +31,24 @@ export default function Button({ children, color, className, ...rest }: ButtonPr
       return 'text-white bg-gray40';
     }
     return color === 'filled'
-      ? 'bg-primary text-white'
+      ? 'bg-primary text-white hover:bg-[#F04F28]'
       : 'bg-white text-primary border border-primary';
   };
 
   return (
-    <div className={`${className}`}>
+    <div className={`relative ${className}`}>
       <button
         onClick={handleClick}
         disabled={disabled}
         className={`flex justify-center items-center rounded-[6px] font-bold text-[12px] md:text-[14px] lg:text-[16px] py-[14px] gap-[8px] w-full h-full ${getColorClass()}`}
         {...restProps}
       >
-        {children}
+        <span className={pending ? 'invisible' : ''}>{children}</span>
+        {pending && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Spinner className="size-[20px]" />
+          </div>
+        )}
       </button>
     </div>
   );
